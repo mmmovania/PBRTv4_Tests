89b7f3d7df56f5fe032d6d6f3367440c9459a068
Muhammad Mobeen Movania <mova0002@e.ntu.edu.sg>
2023-01-07 21:33:53 +0500

    Add files via upload

---
+++ SimpleScene/SimpleScene/SimpleScene.cpp
@@ -0,0 +1,39 @@
+// SimpleScene.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+
+#define PBRT_HAS_INTRIN_H
+#define PBRT_IS_WINDOWS
+#define PBRT_BUILD_GPU_RENDERER
+
+#include <pbrt/pbrt.h>
+#include <pbrt/scene.h> 
+
+#include <pbrt/wavefront/wavefront.h>
+
+using namespace pbrt;
+
+int main()
+{
+    std::vector<std::string> filenames;
+    filenames.push_back("./scenes/simple_scene.pbrt");
+
+    PBRTOptions options;
+    options.useGPU = true;
+    options.interactive = true;
+
+    InitPBRT(options);
+
+    BasicScene scene;
+    BasicSceneBuilder builder(&scene);
+    ParseFiles(&builder, filenames);
+
+    RenderWavefront(scene);
+
+    CleanupPBRT();
+
+
+    return 0;
+} 
